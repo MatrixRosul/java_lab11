be6df0bc6d64c3574bfab920f0a2f8364eed3956
MatrixRosul <rosul.maksym@student.uzhnu.edu.ua>
2024-03-13 21:32:00 +0200

    trird

--- src/Main.java
+++ src/Main.java
@@ -14,40 +14,41 @@ public class Main {
 
             System.out.print("Виберіть опцію: ");
 
-            int choice = scanner.nextInt();
+            String choice = scanner.next();
+
 
             switch (choice) {
-                case 1:
+                case "1":
                     System.out.println("\nДодавання інструменту:");
                     System.out.println("1. Гітара");
                     System.out.println("2. Барабан");
                     System.out.println("3. Бандура");
                     System.out.print("Виберіть інструмент для додавання: ");
-                    int instrumentChoice = scanner.nextInt();
+                    String instrumentChoice = scanner.next();
                     switch (instrumentChoice) {
-                        case 1:
+                        case "1":
                             musician.addInstrument(new Guitar());
                             break;
-                        case 2:
+                        case "2":
                             musician.addInstrument(new Drum());
                             break;
-                        case 3:
+                        case "3":
                             musician.addInstrument(new Bandura());
                             break;
                         default:
                             System.out.println("Некоректний вибір.");
                     }
                     break;
-                case 2:
+                case "2":
                     System.out.println("\nВідтворення інструментів:");
                     musician.playInstrument();
                     break;
-                case 3:
+                case "3":
                     musician = new MusicianImpl();
                     System.out.println("\nСтарого музиканта видалено");
                     System.out.println("\nСтворено нового музиканта");
                     break;
-                case 4:
+                case "4":
                     System.out.println("\nПрограма завершує роботу.");
                     System.exit(0);
                     break;
